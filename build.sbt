778e1afc67dddb09fc7fc0ced6064f12fd239d0f
Ivan Mykhailov <ivan.mvi@gmail.com>
2014-10-08 13:39:48 +0300

    init jersey based project

---
+++ build.sbt
@@ -0,0 +1,33 @@
+name := "jaxrs-demo"
+
+organization := "me.ivanmykhailov"
+
+version := "0.1-SNAPSHOT"
+
+libraryDependencies ++= Seq(
+  "org.glassfish.jersey.containers" % "jersey-container-grizzly2-http" % "2.13"
+)
+
+
+//Tests
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "2.0" % "test"
+)
+
+
+scalacOptions ++= Seq(
+    "-deprecation"
+  , "-feature"
+  , "-unchecked"
+  , "-Xlint"
+  , "-Yno-adapted-args"
+  , "-Ywarn-all"
+  , "-Ywarn-dead-code"
+  , "-language:postfixOps"
+  , "-language:implicitConversions"
+)
+
+//seq(com.github.retronym.SbtOneJar.oneJarSettings: _*)
+
+net.virtualvoid.sbt.graph.Plugin.graphSettings
+
---
+++ src/main/scala/demo/Main.scala
@@ -0,0 +1,30 @@
+package demo;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import java.net.URI;
+
+
+object Main extends App {
+    // Base URI the Grizzly HTTP server will listen on
+    val BASE_URI = "http://localhost:8080/myapp/";
+    
+    def startServer(): HttpServer = {
+        // create a resource config that scans for JAX-RS resources and providers
+        // in com.example package
+        val rc = new ResourceConfig().packages("demo")
+
+        // create and start a new instance of grizzly http server
+        // exposing the Jersey application at BASE_URI
+        GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc)
+    }
+
+    
+    val server = startServer();
+    println(s"Jersey app started with WADL available at ${BASE_URI}sapplication.wadl Hit enter to stop it...")
+    readLine()
+    server.stop()
+    
+}
+
